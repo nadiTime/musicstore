268bcd688bf4c547714c1925bf310147c5d64a22
nadav cohen <nadavcn@gmail.com>
2015-09-15 16:21:49 +0300

    deprecate unused var wishlist controller

--- client/scripts/wishlist_page/wishlist.controller.js
+++ client/scripts/wishlist_page/wishlist.controller.js
@@ -5,26 +5,22 @@
 		.controller('WishlistController' , ['$scope' , 'GeneralFactory' , 'WishlistFactory' ,
 			function($scope,GeneralFactory,WishlistFactory){
 				$scope.wishlist = [];
-
 				var init = function(){
 					var assets_images = '/musicstore/assets/images/';
-					var albums_id_obj = GeneralFactory.getFromLS('wishlist');
-					console.log(albums_id_obj);
-					var arr = [];
-					angular.forEach(albums_id_obj, function(el){
-						arr.push(el);
-					});
-					// console.log(arr);
-					WishlistFactory.getWishlist(arr)
-					.then(function(res_albums_obj){
-						angular.forEach(res_albums_obj , function(album){
-							album.image_path = assets_images.concat(album.image_path);
-							$scope.wishlist.push(album);
+					var arr = GeneralFactory.getFromLS('wishlist');
+					if( arr.length > 0){
+						WishlistFactory.getWishlist(arr)
+						.then(function(res_albums_obj){
+							angular.forEach(res_albums_obj , function(album){
+								album.image_path = assets_images.concat(album.image_path);
+								$scope.wishlist.push(album);
+							});
 						});
-					});
+					}
+					else{
+						//display page for empty wishlist
+					}
 				}
-
-
 				init();
 			}]);
 })();
