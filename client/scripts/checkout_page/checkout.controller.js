3921eeb17ee5ae03990ad16bbc48536c5649c652
ido rapaport <idorapaport@gmail.com>
2015-09-16 17:16:20 +0300

    orginaze accordion view

--- client/scripts/checkout_page/checkout.controller.js
+++ client/scripts/checkout_page/checkout.controller.js
@@ -4,12 +4,27 @@
 	angular.module('musicstore.checkout')
 		.controller('CheckoutController' , ['$scope' , '$rootScope' , 'GeneralFactory' ,
 			function($scope,$rootScope,GeneralFactory){
+				$scope.cart = {};
+				$scope.details = [];
+				$scope.bill_address = '';
+				$scope.bill_city = '';
+				$scope.bill_zipcode = '';
+				$scope.bill_tel = '';
 
 				var init = function(){
-					var cart = GeneralFactory.getFromLS('cart');
-					console.log(cart);
-
+					$scope.cart = GeneralFactory.getFromLS('cart');
 				}; 
+
+				$scope.fetchInfo = function(){
+					var add = $scope.bill_address;
+					var city = $scope.bill_city;
+					var zip = $scope.bill_zipcode;
+					var tel = $scope.bill_tel;
+					$scope.details.push(add,city,zip,tel);
+					$scope.accordion = $scope.accordion +1;
+					console.log($scope.details);
+				}
+
 				init();
 		}]);
 })();
