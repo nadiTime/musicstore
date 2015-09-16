ebb942520d552213c15c937d67b6ae1b7ec8228f
ido rapaport <idorapaport@gmail.com>
2015-09-16 20:33:27 +0300

    credit card submit valdation

--- client/scripts/checkout_page/checkout.controller.js
+++ client/scripts/checkout_page/checkout.controller.js
@@ -6,6 +6,8 @@
 			function($scope,$rootScope,GeneralFactory){
 				$scope.cart = {};
 				$scope.details = [];
+				$scope.months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+				$scope.years = ['2016','2017','2018','2019','2020','2021'];
 				
 
 				$scope.bill_address = '';
@@ -32,7 +34,7 @@
 					var zip = GeneralFactory.Validate.inputt($scope.bill_zipcode);
 					var tel = GeneralFactory.Validate.inputt($scope.bill_tel);
 
-					if (add & city & zip & tel) {
+					if (add.length > 0 & city.length > 0 & zip.length > 0 & tel.length > 0) {
 						$scope.details.push(add,city,zip,tel);
 						$scope.accordion = $scope.accordion +1;
 					}else {
@@ -44,7 +46,19 @@
 				$scope.cardInfoSubmit = function(){
 					var card_type = GeneralFactory.Validate.inputt($scope.card_type.single_select);
 					var card_holder = GeneralFactory.Validate.inputt($scope.card_holder);
-					var card_number = GeneralFactory.Validate.creditCard()
+					var card_number = GeneralFactory.Validate.inputt($scope.card_number);
+
+					if (!card_type & !card_holder & !card_number) {
+						alert('one or more of the credit card details is empty');
+					}
+
+					var card_number = GeneralFactory.Validate.creditCard(card_number,card_type);
+
+					if (!card_number) {
+						alert('invalid card number');
+					}
+
+
 				}
 
 				init();
