e015916afd8c3c2c48749122e72a4b5b41ea5ae2
nadav cohen <nadavcn@gmail.com>
2015-09-19 20:34:05 +0300

    finish

--- client/scripts/directives/cart_list/cartlist.controller.js
+++ client/scripts/directives/cart_list/cartlist.controller.js
@@ -2,23 +2,36 @@
 	'use strict';
 
 	angular.module('musicstore.cartlist')
-		.controller('CartlistController' , ['$scope' , '$rootScope' , 'GeneralFactory' , 'CheckoutFactory',
-			function($scope,$rootScope,GeneralFactory,CheckoutFactory){
+		.controller('CartlistController' , ['$location','$scope' , '$rootScope' , 'GeneralFactory' , 'CheckoutFactory',
+			function($location,$scope,$rootScope,GeneralFactory,CheckoutFactory){
 
 			$scope.cart = [];
 			var assets_images = '/musicstore/assets/images/';
-
+			$scope.cart_subtotal = 0;
+			$scope.goToCheckout = function(){
+				$location.path('/checkout/');
+			}
 			var init = function(){
 				var cart = GeneralFactory.getFromLS('cart');
 				console.log(cart);
-				var albums_obj = cart.albums;
-				CheckoutFactory.getAlbums(albums_obj)
+				if(typeof cart != 'null'){
+					var albums_obj = cart.albums;
+					CheckoutFactory.getAlbums(albums_obj)
 					.then(function(response){
-						$scope.cart.push(response);		
-					});
-					
+						console.log(response);
+						var amount_for_album = 0;
+						var i = 0;
+						var image_path = 'assets/images/';
+						angular.forEach(response,function(album){
+							response[i].amount = cart.amount[i];
+							response[i].image_path = image_path + response[i].image_path; 
+							i++;
+							$scope.cart_subtotal += (cart.amount[i]*album.album_price);
+						});
+						$scope.cart = response;	
+					});	
 				}
-					
+			}		
 			init();
 		}]);
 })();
